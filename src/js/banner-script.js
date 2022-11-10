2565ebaffa564502b655cd470134e69488db0e6e
https://github.com/SaulAguinaga <saul_aguinaga@hotmail.es>
2022-11-10 12:18:13 +0100

    ended banner

--- src/js/banner-script.js
+++ src/js/banner-script.js
@@ -5,11 +5,15 @@ carousel();
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 4000);
+  
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+      slideIndex++;
+
+    if (slideIndex > x.length) {
+      slideIndex = 1
+    }
+      x[slideIndex-1].style.display = "block";
+      setTimeout(carousel, 4000);
 }
